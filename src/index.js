46375b65cccce9ccdab60c3599f65dfc123c6c26
winter <jsylego@naver.com>
2022-05-22 11:59:50 +0900

    Removed psl package
    Replaced with express' native property

--- src/index.js
+++ src/index.js
@@ -1,41 +1,37 @@
 const path = require('path')
 let express = require('express')
 let app = express()
-let psl = require('psl')
-// let router = express.Router()
-// let favicon = require('serve-favicon')
 
-// app.use('/:faviconname', (req, res, next) => {
-//   if (req.params.faviconname.indexOf('\0') !== -1) {
-//     res.status(404).json({})
-//   }
-//   let rootDirectory = path.join(__dirname, 'svg')
-//   let filename = req.params.faviconname + '.svg'
-//   let filedir = path.join(rootDirectory, filename)
-//   if (filedir.indexOf(rootDirectory) !== 0) {
-//     res.status(404).json({})
-//   }
-//   favicon(filedir)
-//   next()
-// })
+let logger = (req, res, next) => {
+  res.on('finish', () => {
+    console.info(
+      `${new Date().toISOString()}: method=${req.method} path="${
+        req.path
+      }" host=${req.hostname} fwd=${req.ips} protocol=${req.protocol}`
+    )
+  })
+  next()
+}
 
 app.use(express.static(path.join(__dirname, 'favicon')))
 app.set('view engine', 'ejs')
+app.use(logger)
 
 app.get('/', (req, res) => {
-
   if (req.hostname.indexOf('\0') !== -1) {
-    res.status(404).json({})
+    return res.status(418)
   }
-  let parsed = psl.parse(req.hostname)
-  if (parsed.subdomain === null) {
-    return res.status(200).render('favicon', { filename: 'burger'})
+  if (req.subdomains.length === 0) {
+    return res.status(200).send(`Use &lt;favicon name&gt;.${req.hostname}`)
   }
-  let hostname = parsed.subdomain.split('.')
-  res.status(200).render('favicon', { filename: hostname[hostname.length - 1]})
+  res.status(200).render('favicon', { filename: req.subdomains[0] })
 })
 
-var port = process.env.PORT || 8080 // 사용할 포트 번호를 port 변수에 넣습니다.
+app.get('/ping', (req, res) => {
+  res.status(204)
+})
+
+var port = process.env.PORT || 8080
 app.listen(port, function () {
-  console.log('INFO:: Server started at http://localhost:' + port)
+  console.info('Listening at http://localhost:' + port)
 })
